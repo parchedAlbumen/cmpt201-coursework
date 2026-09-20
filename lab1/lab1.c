e284f38b20487f9f8bbbbf0f1ff25d7766aeea1a
Kenneth Pamintuan <kpp4@sfu.ca>
2026-09-20 16:31:29 -0700

    lab 1 work

--- lab1/lab1.c
+++ lab1/lab1.c
@@ -4,23 +4,27 @@
 #include <string.h>
 
 int main() {
-  printf("hello world!!!\n");
-  printf("enter something here:");
+  printf("enter something here: ");
   char *sentence = NULL;
   size_t len = 0;
   ssize_t num_char = getline(&sentence, &len, stdin);
-  if (num_char == -1) {
-    perror("getline failed!");
-    exit(EXIT_FAILURE);
-  }
-  printf("%s\n", sentence);
+  while (num_char != -1) {
+    if (num_char == -1) {
+      perror("getline failed!");
+      exit(EXIT_FAILURE);
+    }
+    printf("Tokens: \n");
 
-  // is it supposed to be like this ?
-  char *saveptr;
-  char *ret = strtok_r(sentence, " ", &saveptr);
-  while (ret != NULL) {
-    printf("%s\n", ret);
-    ret = strtok_r(NULL, " ", &saveptr);
+    // is it supposed to be like this ?
+    char *saveptr;
+    char *ret = strtok_r(sentence, " ", &saveptr);
+    while (ret != NULL) {
+      printf(" %s\n", ret);
+      ret = strtok_r(NULL, " ", &saveptr);
+    }
+    printf("enter something here: ");
+    num_char = getline(&sentence, &len, stdin);
   }
+
   free(sentence);
 }
